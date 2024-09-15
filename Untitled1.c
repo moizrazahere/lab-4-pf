d9c97ff22339fe7cf4171c92d8ba5c291b1e9bb2
moizrazahere <moizrazakhan72@gmail.com>
2024-09-15 23:52:13 +0500

    Add files via upload

---
+++ Untitled1.c
@@ -0,0 +1,14 @@
+//LAB 4 Q1 24K 1011
+#include<stdio.h> //library
+
+int main() {
+	int a;
+	printf("enter a number");
+	scanf("%d",&a);
+if  (a%3==0){
+	printf("number is multiple of 3");  
+}
+else{
+ printf("number is not multiple of three");
+}
+}
